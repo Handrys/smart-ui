83930ab3829f51a00aa5adaa3e79d2a3baa2d610
Handrys <71773865+Handrys@users.noreply.github.com>
2021-08-10 11:24:19 +0300

    n

---
+++ js/images.js
@@ -0,0 +1,43 @@
+const images = {
+    'dishes_italian' : {
+        'name' : 'ITALIAN',
+        'value' : '327 Recipes'
+    },
+    'dishes_indian' : {
+        'name' : 'INDIAN',
+        'value' : '327 Recipes'
+    },
+    'dishes_french' : {
+        'name' : 'FRENCH',
+        'value' : '327 Recipes'
+    },
+    'dishes_steakhouse' : {
+        'name' : 'STEAKHOUSE',
+        'value' : '327 Recipes'
+    },
+    'dishes_seafood' : {
+        'name' : 'SEAFOOD',
+        'value' : '327 Recipes'
+    },
+    'dishes_sushi' : {
+        'name' : 'SUSHI',
+        'value' : '327 Recipes'
+    },
+    'dishes_mexican' : {
+        'name' : 'MEXICAN',
+        'value' : '327 Recipes'
+    },
+    'dishes_chinese' : {
+        'name' : 'CHINESE',
+        'value' : '327 Recipes'
+    },
+    'dishes_pizza' : {
+        'name' : 'PIZZA',
+        'value' : '327 Recipes'
+    },
+    'dishes_american' : {
+        'name' : 'AMERICAN',
+        'value' : '327 Recipes'
+    },
+
+}
